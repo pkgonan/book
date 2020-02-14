ab45aa2a9b88ffb1061155643ca1dc49c5acb367
pkgonan <pkgonan@yanolja.com>
2020-02-14 18:16:03 +0900

    [ADD] init

---
+++ README.md
@@ -0,0 +1,22 @@
+# Book Search Service Study
+
+## How to use ?
+* Clone project
+
+
+        git clone https://github.com/pkgonan/book.git
+
+
+* Initialize
+
+
+        docker-compose up -d
+
+
+## Feature
+* Join
+* Login
+* Book Search
+* My Search History
+* Popular Search Keyword
+
---
+++ src/main/java/io/book/config/RedisConfig.java
@@ -0,0 +1,54 @@
+package io.book.config;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.TimeoutOptions;
+import io.lettuce.core.cluster.ClusterClientOptions;
+import io.lettuce.core.cluster.ClusterTopologyRefreshOptions;
+import org.springframework.boot.autoconfigure.data.redis.LettuceClientConfigurationBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+import static io.lettuce.core.ReadFrom.REPLICA_PREFERRED;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public ReactiveRedisTemplate<String, Long> stringLongReactiveRedisTemplate(final ReactiveRedisConnectionFactory connectionFactory) {
+        final RedisSerializer<String> serializer = new StringRedisSerializer();
+        final Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Long.class);
+        final RedisSerializationContext serializationContext = RedisSerializationContext
+                .<String, Long>newSerializationContext()
+                .key(serializer)
+                .value(jackson2JsonRedisSerializer)
+                .hashKey(serializer)
+                .hashValue(jackson2JsonRedisSerializer)
+                .build();
+
+        return new ReactiveRedisTemplate<String, Long>(connectionFactory, serializationContext);
+    }
+
+    @Bean
+    public LettuceClientConfigurationBuilderCustomizer lettuceClientConfigurationBuilderCustomizer() {
+        return builder -> builder
+                .readFrom(REPLICA_PREFERRED)
+                .commandTimeout(Duration.ofSeconds(1L))
+                .clientOptions(getClientOptions());
+    }
+
+    private ClientOptions getClientOptions() {
+        return ClusterClientOptions.builder()
+                .topologyRefreshOptions(ClusterTopologyRefreshOptions.enabled())
+                .autoReconnect(true)
+                .timeoutOptions(TimeoutOptions.builder().fixedTimeout(Duration.ofSeconds(1L)).build())
+                .build();
+    }
+}
